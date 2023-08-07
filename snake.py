f10a628be06590cfce23248235b123edb691fd18
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-07 14:12:51 -0400

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,61 +1,52 @@
 from turtle import Turtle
-START_POS = [(0,0), (-20,0), (-40,0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
-LEFT = 180
 DOWN = 270
+LEFT = 180
 RIGHT = 0
 
+
 class Snake:
-  def __init__(self):
-    self.segments = []
-    self.create_snake()
-    self.head = self.segments[0]
 
-  # create_snake, add_segment, grow interesting
-  def create_snake(self):
-    for position in START_POS:
-      self.add_segment(position)
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
 
-  # add new segment to snake
-  def add_segment(self, position):
-    snake_seg = Turtle()
-    snake_seg.shape("square")
-    snake_seg.color("white")
-    snake_seg.penup()
-    snake_seg.goto(position)
-    self.segments.append(snake_seg)
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
 
-  def grow(self):
-    # in python use negative number to start from end of list
-    # position is method in turtle class
-    self.add_segment(self.segments[-1].position())
-  
-  def move(self):
-    for seg_num in range(len(self.segments) -1, 0, -1):
-      new_x = self.segments[seg_num -1].xcor()
-      new_y = self.segments[seg_num -1].ycor()
-      self.segments[seg_num].goto(new_x, new_y)
-    self.segments[0].forward(MOVE_DISTANCE)
-  
-  def up(self):
-    #snake not allowed to go back on itself
-    # why does self.head.heading() work
-    if self.head.heading() != DOWN:
-      self.head.setheading(UP)
-      self.move()
-    
-  def down(self):
-    if self.head.heading() != UP:
-      self.head.setheading(DOWN)
-      self.move()
-  
-  def left(self):
-    if self.head.heading() != RIGHT:
-      self.head.setheading(LEFT)
-      self.move()
-  
-  def right(self):
-    if self.head.heading() != LEFT:
-      self.head.setheading(RIGHT)
-      self.move()
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
