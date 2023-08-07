cb622b38e0e1625091bd8f1dc18c7f7bd0422859
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-07 14:12:19 -0400

    Update score.py

--- score.py
+++ score.py
@@ -1,32 +1,27 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = ('Arial', 10, 'normal')
+FONT = ("Courier", 24, "normal")
 
-class Score(Turtle):
 
-  # inheritance: snake is a turtle, food is a food which inherits from turtle
-  def __init__(self):
-    super().__init__()
-    self.score = 0
-    self.hideturtle()
-    self.penup()
-    self.pencolor("white")
-    self.setposition(-270, 270)
-    self.display()
+class Scoreboard(Turtle):
 
-  def display(self):
-    self.clear()
-    self.write(f"Score: {self.score}", False, align = ALIGNMENT, font = FONT)
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
 
-  def keep_score(self):
-    self.score += 1
-    self.display()
-    # print(self.score)
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
-  def game_over(self):
-    self.goto(0,0)
-    self.write("GAME OVER", align = ALIGNMENT, font = FONT)
-  # def refresh(self):
-  #   self.setposition(-0, 270)
-  #   # self.display()
-    
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
