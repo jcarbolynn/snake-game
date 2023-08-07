06a0726e2c14c42e87eec62e484b516cd107ca8d
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-07 14:11:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,52 @@
-from turtle import Turtle, Screen
-# from snake import Snake
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
-# when tracer is off (0) nothing prints, screen wont refresh until you call update
+screen.title("My Snake Game")
 screen.tracer(0)
 
-segments = []
-for _ in range(3):
-  snake_seg = Turtle()
-  snake_seg.shape("square")
-  snake_seg.color("white")
-  # snake_seg.setx(10-(_*20))
-  snake_seg.penup()
-  snake_seg.setx(-(_*20))
-  segments.append(snake_seg)
-# update after snake printed so it will not show up piece by piece, all displayed at once
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
-game_on = True
-while game_on:
-  #move each segment, loop through each segment and move it forward by an ammount
-  screen.update() # snake moves as one piece, all 3 segments forward before update triggered
-  # move delay up here so only delays after all 3 pieces moved forward
-  time.sleep(.1)
-  # for seg in segments:
-  #   seg.forward(10)
-  #   # # adds 1s delay after each piece moves
-  #   # time.sleep(1)
-  # replace above for loop with below because above only lets snake move forward, instead have each segment move to where segment before was
-  # for seg_num in range(start=len(segments) -1, stop=0, step= -1):
-  for seg_num in range(len(segments) -1, 0, -1):
-    new_x = segments[seg_num -1].xcor()
-    new_y = segments[seg_num -1].ycor()
-    segments[seg_num].goto(new_x, new_y)
 
-  segments[0].forward(20)
-  
+
 
 screen.exitonclick()
